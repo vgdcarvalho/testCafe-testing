612ff1b1b52b58ceae33bf5c2abb4236282b4955
Vinicius Carvalho <vinicius.carvalho@vistajet.com>
2021-03-24 18:13:55 -0300

    Adicionando test sped, selectors, screenshots e timeouts

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -10,7 +10,12 @@ fixture `Getting started with TestCafe`
     })
     .beforeEach(async t => {
         // Runs before each test
-        await t.setTestSpeed(1)
+
+        //// Set the speed of the test: float 0 < x < 1
+        //await t.setTestSpeed(1)
+
+        //// Set timeout of TestCafe waiting window.load event 
+        //await t.setPageLoadTimeout(1)
     })
     .after(async t => {
         // Cleaning test data
@@ -21,9 +26,34 @@ fixture `Getting started with TestCafe`
     })
 
 test('My first TestCafe test', async t => {
-    //here goes testcafe code
-    await t.typeText('#developer-name', 'David')
-    await t.click('#submit-button')
+    // here goes testcafe code
 
-    await t.expect(Selector('#article-header').innerText).contains('David')
+    // Without Selector:
+    // await t.typeText('#developer-name', 'David')
+    //// await t.wait(3000)
+    // await t.click('#submit-button')
+
+    // await t.expect(Selector('#article-header').innerText).contains('David')
+
+    // With Selector:
+    const developer_name_input = Selector('#developer-name')
+    const submit_button = Selector('#submit-button')
+    const articleText = Selector('#article-header').innerText
+
+    // Take screenshot, several ways:
+    //// Specifying path
+    //await t.takeScreenshot({path: "./tests/test.png"})
+    //// Using default path and capturing the whole page
+    //await t.takeScreenshot({fullPage: true})
+    //// Taking screenshot of an specific element
+    //await t.takeElementScreenshot(submit_button)
+
+    // Type in text field:
+    await t.typeText(developer_name_input, 'David')
+    // await t.wait(3000)
+
+    // Click on button:
+    await t.click(submit_button)
+    // Check if the text displayd is correct:
+    await t.expect(articleText).contains('David')
 })
