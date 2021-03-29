3964cff38d28159f7fe4c674da18ec11ea92dc69
Vinicius Carvalho <vinicius.carvalho@vistajet.com>
2021-03-29 17:36:33 -0300

    Adcionando novo teste que ainda precisa ser consertado

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,15 +1,17 @@
-import { Selector } from 'testcafe'
+import { Selector, t } from 'testcafe'
 
 fixture `Login Test`
     .page `http://zero.webappsecurity.com/index.html`
 
-    test("User cannot login with invalid credentials", async t => {
+    .beforeEach(async t => {
         const singInBtn = Selector('#signin_button')
         await t.click(singInBtn)
 
         const loginForm = Selector('#login_form')
         await t.expect(loginForm.exists).ok()
+    })
 
+    test("User cannot login with invalid credentials", async t => {
         const usernameInput = Selector('#user_login')
         const passwordInput = Selector('#user_password')
         await t.typeText(usernameInput, 'invalid user', { paste: true })
@@ -20,4 +22,18 @@ fixture `Login Test`
 
         const errorMsg = Selector('.alert-error').innerText
         await t.expect(errorMsg).contains('Login and/or password are wrong.')
+    })
+
+    test("User can login with valid credentials", async t => {
+        const usernameInput = Selector('#user_login')
+        const passwordInput = Selector('#user_password')
+        await t.typeText(usernameInput, 'username', { paste: true })
+        await t.typeText(passwordInput, 'password', { paste: true })
+
+        const submitBtn = Selector('.btn-primary')
+        await t.click(submitBtn)
+
+        const accountTab = Selector('#account_summary_tab')
+        await t.expect(accountTab.exists).ok()
+        await t.expect(loginForm.exists).notOk()
     })
